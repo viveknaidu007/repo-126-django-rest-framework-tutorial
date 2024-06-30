675b8691297fca6db645eb206c28e626c7cb47d7
viveknaidu007 <vivek.pavankalyan@gmail.com>
2024-06-30 22:00:43 +0530

    updated

---
+++ 10.authentication/Books/Book/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
+from rest_framework import status
+from rest_framework import viewsets
+
+class Studentviewset(viewsets.ModelViewSet):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
---
+++ 10.authentication/Books/Books/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path , include
+from Book import views
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+router.register("studentapi",views.Studentviewset,basename="student")
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("",include(router.urls)),
+]
