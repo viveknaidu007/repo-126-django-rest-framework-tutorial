7da3d4b70040cbf3e38ef9d75a3a3822e0f06b7e
viveknaidu007 <vivek.pavankalyan@gmail.com>
2024-06-30 19:59:33 +0530

    updated

--- .history/8.View_sets/Books/Book/serializers_20240630154401.py
+++
@@ -1,7 +0,0 @@
-from rest_frameowork import serializers
-from .models import *
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Student
-        fields = "__all__"
--- .history/88.view_sets/Books/Book/serializers_20240629224706.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-class StudentSerializers(serializers.ModelSerializer):
-    class Meta:
-        model=Student
-        fields="__all__"
